dd6bf46ad38b2d7e5f89a39cd3f4cae7be6dd0e3
JoJACO <contrerasjorge226@gmail.com>
2023-06-27 18:10:26 -0700

    Started on the layout. Added a text/textField/button. no functionality at the moment and everything is hard coded

--- app/src/main/java/com/example/moneyspent/MainActivity.kt
+++ app/src/main/java/com/example/moneyspent/MainActivity.kt
@@ -3,12 +3,20 @@ package com.example.moneyspent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.moneyspent.ui.theme.MoneySpentTheme
 
@@ -22,25 +30,44 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MoneySpentLayout()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MoneySpentLayout(modifier: Modifier = Modifier){
+
+    Column(modifier = modifier) {
+        Text(text = "Your Spending")
+        //
+        TextField(value = "6.69",
+            onValueChange = {},
+            modifier = modifier)
+        //
+        EnterButton()
+
+
+
+    }
+
+}
+
+@Composable
+fun EnterButton(){
+    Button(onClick = { /*TODO*/ }) {
+        Text(text = "Enter")
+        
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MoneySpentPreview() {
     MoneySpentTheme {
-        Greeting("Android")
+        MoneySpentLayout()
     }
 }
