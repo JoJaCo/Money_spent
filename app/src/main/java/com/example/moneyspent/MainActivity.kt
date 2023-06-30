a5de165ccc5a224283f1c59049d1d18444a07e79
JoJACO <contrerasjorge226@gmail.com>
2023-06-29 21:32:21 -0700

    Extracted the strings and fixed the layout. Goind to add functionality to the button and the textfield.

--- app/src/main/java/com/example/moneyspent/MainActivity.kt
+++ app/src/main/java/com/example/moneyspent/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
@@ -25,6 +26,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.InspectableModifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -58,15 +60,16 @@ fun MoneySpentLayout(
     horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Row(modifier = modifier,
+        Row(modifier = modifier.fillMaxWidth(),
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically) {
 
             Text(modifier = modifier
                 .padding(start = 55.dp)
-                , text = "Your Spending:")
+                , text = stringResource(R.string.spending))
+
             Text(modifier = modifier
-                .padding(start = 0.dp)
+                .padding(end = 60.dp)
                 , text = "6.69")
         }
 
