f9404d56346cccc46271473c079c60a9d7ab5ad4
JoJACO <contrerasjorge226@gmail.com>
2023-07-01 06:43:06 -0700

    started working on state. Will be working on button functionality and keyboard input

--- app/src/main/java/com/example/moneyspent/MainActivity.kt
+++ app/src/main/java/com/example/moneyspent/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -21,12 +22,13 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.InspectableModifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -55,59 +57,68 @@ fun MoneySpentLayout(
     modifier: Modifier = Modifier
 
 ) {
+    var textFieldState by remember {
+        mutableStateOf("0.00")
+    }
 
 
 
-    Column(modifier = modifier,
-    horizontalAlignment = Alignment.CenterHorizontally,
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Row(modifier = modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.SpaceBetween,
-        verticalAlignment = Alignment.CenterVertically) {
+        Row(
+            modifier = modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
 
-            Text(modifier = modifier
-                .padding(start = 55.dp)
-                , text = stringResource(R.string.spending))
+            Text(
+                modifier = modifier
+                    .padding(start = 55.dp), text = stringResource(R.string.spending)
+            )
 
-            Text(modifier = modifier
-                .padding(end = 60.dp)
-                , text = "6.69")
+
+            // what you spent
+            Text(
+                modifier = modifier
+                    .padding(end = 60.dp), text = textFieldState
+            )
         }
 
         //
         Spacer(modifier = modifier.padding(10.dp))
-
+        //user input
         TextField(
-            value = "6.69",
-            onValueChange = {},
+            value = textFieldState,
+            label = {
+                Text(text = "Enter a amount")
+            },
+            onValueChange = {
+                textFieldState = it
+            },
+            singleLine = true,
             modifier = modifier
         )
         Spacer(modifier = modifier.padding(10.dp))
         //
-        EnterButtonAndTextField(modifier = modifier)
+        Button(onClick = {
 
 
-
-
+        }) {
+            Text(text = "Click ME")
+        }
 
     }
 
 }
 
-@Composable
-fun EnterButtonAndTextField(modifier: Modifier = Modifier,
-onButtonClick:() -> Unit
-) {
 
-    Button(onClick = onButtonClick) {
-        Text(text = stringResource(R.string.enter))
-
-    }
-}
-
-@Preview(showBackground = true,
-showSystemUi = true)
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
 @Composable
 fun MoneySpentPreview() {
     MoneySpentTheme {
