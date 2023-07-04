55ad348176f4cb9922a6dba3a208983c61e68c0c
JoJACO <contrerasjorge226@gmail.com>
2023-07-04 14:41:43 -0700

    worked on recomposing the textfield to better work with state flow. need to add some material design.

--- app/src/main/java/com/example/moneyspent/MainActivity.kt
+++ app/src/main/java/com/example/moneyspent/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.moneyspent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.animation.AnimatedContentScope.SlideDirection.Companion.End
 import androidx.compose.foundation.Image
@@ -17,6 +18,7 @@ import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -29,6 +31,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.InspectableModifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -54,55 +57,53 @@ class MainActivity : ComponentActivity() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MoneySpentLayout(
-    modifier: Modifier = Modifier
-
-) {
-    var textFieldState by remember {
-        mutableStateOf("0.00")
+fun MoneySpentLayout() {
+    var amountInput by remember {
+       mutableStateOf("0.00")
+    val amount = amountInput.toDoubleOrNull() ?: 0.0
     }
 
 
 
     Column(
-        modifier = modifier,
+        modifier = Modifier,
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
         Row(
-            modifier = modifier.fillMaxWidth(),
+            modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically
         ) {
 
             Text(
-                modifier = modifier
+                modifier = Modifier
                     .padding(start = 55.dp), text = stringResource(R.string.spending)
             )
 
 
             // what you spent
             Text(
-                modifier = modifier
+                modifier = Modifier
                     .padding(end = 60.dp), text = textFieldState
             )
         }
 
         //
-        Spacer(modifier = modifier.padding(10.dp))
+        Spacer(modifier = Modifier.padding(10.dp))
         //user input
-        TextField(
-            value = textFieldState,
-            label = {
-                Text(text = "Enter a amount")
-            },
-            onValueChange = {
-                textFieldState = it
-            },
-            singleLine = true,
-            modifier = modifier
+        EditNumberField(
+            label = R.string.enter,
+            leadingIcon = R.drawable.money,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            value = amountInput,
+            onValueChange = { amountInput = it}
+
         )
-        Spacer(modifier = modifier.padding(10.dp))
+        Spacer(modifier = Modifier.padding(10.dp))
         //
         Button(onClick = {
 
@@ -114,20 +115,31 @@ fun MoneySpentLayout(
     }
 
 }
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EditNumberField(
     @StringRes label: Int,
-    value:String,
+    @DrawableRes leadingIcon: Int,
+    keyboardOptions: KeyboardOptions,
+    value: String,
     onValueChange: (String) -> Unit,
     modifier: Modifier = Modifier
 
-){
-    TextField(value = value,
-        onValueChange = onValueChange)
+) {
+    TextField(
+        value = value,
+        leadingIcon = {
+            Icon(
+                painter = painterResource(id = leadingIcon),
+                contentDescription = null
+            )
+        },
+        onValueChange = onValueChange,
+        label = { Text(text = stringResource(label))},
+        keyboardOptions = keyboardOptions
+    )
 }
-
-
 
 
 @Preview(
